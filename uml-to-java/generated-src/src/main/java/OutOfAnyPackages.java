5284b788c9b744c1001930100f48a211e2f616f7
Craig Gallen <cgallen@opennms.org>
2019-06-10 23:52:37 +0100

    papyrus uml 2 java works

---
+++ uml-to-java/generated-src/src/main/java/OutOfAnyPackages.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * All rights reserved.
+ *******************************************************************************/
+
+
+// Start of user code (user defined imports)
+
+// End of user code
+
+/**
+ * Description of OutOfAnyPackages.
+ * 
+ * @author sbegaudeau
+ */
+
+
+}
