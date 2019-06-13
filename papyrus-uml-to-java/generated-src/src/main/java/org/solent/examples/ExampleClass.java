e01c9cbecd23bd8e72bd706ca68c73d18583d959
Craig Gallen <Craig Gallen>
2019-06-13 20:46:45 +0100

    added model - not generating in maven

---
+++ papyrus-uml-to-java/generated-src/src/main/java/org/solent/examples/ExampleClass.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * All rights reserved.
+ *******************************************************************************/
+package org.solent.examples;
+
+// Start of user code (user defined imports)
+
+// End of user code
+
+/**
+ * Description of ExampleClass.
+ * 
+ * @author sbegaudeau
+ */
+
+
+}
